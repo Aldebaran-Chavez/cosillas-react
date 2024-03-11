4fcf9bbc22b71b76aa6db9085ba9fccb53345ba5
Diego Aldebaran <145601953+Aldebaran-Chavez@users.noreply.github.com>
2024-03-10 22:29:03 -0600

    card

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Card from './components/Card'
+
+// borre lo que no ocupo
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hola Buenas Tardes soy Reactivo
+      </div>
+
+      <p></p>
+
+      <Card />
+      
+      <p></p>
+
+      <Card />
+
+    </>
+  )
+}
+
+export default App
---
+++ components/Card.jsx
@@ -0,0 +1,14 @@
+import "./Card.css";
+
+function Card() {
+    return (<div className="card">
+        
+        <h2>Titulo de tarjeta</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Non tempora eius consequuntur ratione placeat magni</p>
+
+    </div> );
+}
+
+export default Card;
